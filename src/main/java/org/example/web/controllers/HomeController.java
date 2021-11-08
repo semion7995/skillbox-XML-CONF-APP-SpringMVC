32f6ad2859aed7f232c7c06d1defa232a9c0e6a3
Semen <semaqwinnert@gmail.com>
2021-11-08 21:18:29 +0300

    first

---
+++ src/main/java/org/example/web/controllers/HomeController.java
@@ -0,0 +1,21 @@
+package org.example.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import org.apache.log4j.Logger;
+
+@Controller
+public class HomeController {
+
+    private Logger logger = Logger.getLogger(HomeController.class);
+
+
+    @RequestMapping(value = "/home", method = RequestMethod.GET)
+    public ModelAndView home(){
+        logger.info("GET /home returns index.html");
+        return new ModelAndView("index");
+    }
+}
